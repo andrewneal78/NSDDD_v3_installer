7c6737c30dffe77638974f3469807bd484ecf34b
Andrew Neal <andrew.neal@ed.ac.uk>
2026-02-06 11:14:45 +0000

    Fix ImportError and truncated checkbox labels
    - Fix utils/__init__.py: display_directory_tree -> display_directory_structure
    - Fix checkbox labels: Add layout width 400px to prevent truncation
    - All checkboxes now display full text

--- utils/__init__.py
+++ utils/__init__.py
@@ -7,7 +7,7 @@ Utilities for downloading, verifying, and setting up the NSDDD v3 dataset.
 from .datashare import DataShareClient
 from .download import download_file, calculate_total_size
 from .verify import verify_file, calculate_sha256
-from .setup import create_directory_structure, display_directory_tree
+from .setup import create_directory_structure, display_directory_structure
 
 __all__ = [
     'DataShareClient',
@@ -16,5 +16,5 @@ __all__ = [
     'verify_file',
     'calculate_sha256',
     'create_directory_structure',
-    'display_directory_tree'
+    'display_directory_structure'
 ]
